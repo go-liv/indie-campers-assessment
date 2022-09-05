590cd9b2923a4aa31bfc1589c6aaaedecbb3c40f
Guilherme <go.livtech@gmail.com>
2022-09-05 11:18:21 +0100

    Adição de comentários relevantes e algum error handling.

--- part2/controllers/methods.js
+++ part2/controllers/methods.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 
 module.exports.getDirections = async (dep, arr) => {
     let data = null;
+    // The Directions API responds with an object where we can find the available routes 
+    // (in this case we ask only for one but alternatives could be asked for), 
+    // inside the object we can find all the coordinates that compose the directions between dep and arr on object.routes[index] 
     await axios.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${dep};${arr}?geometries=geojson&access_token=pk.eyJ1IjoiZmlzaHN0aXh6IiwiYSI6ImNrems3b2p0MzIzZmgybm9jcjVzMHZ1aHoifQ.s6imzJ1hBpWdfdJzwmehOQ`)
         .then(response => {
             data = response.data;
@@ -14,6 +17,8 @@ module.exports.getDirections = async (dep, arr) => {
 
 module.exports.getHighlights = async (lng, lat) => {
     let data = null;
+    // The Tilequery API can provide more than one feature, so all features under a 50m radius of the 
+    // coordinate will be returned to be added to a list on the route
     await axios.get(`https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/${lng},${lat}.json?radius=50&layers=poi_label&access_token=pk.eyJ1IjoiZmlzaHN0aXh6IiwiYSI6ImNrems3b2p0MzIzZmgybm9jcjVzMHZ1aHoifQ.s6imzJ1hBpWdfdJzwmehOQ`)
         .then(response => {
             data = response.data.features;
@@ -25,7 +30,8 @@ module.exports.getHighlights = async (lng, lat) => {
 };
 
 module.exports.closestHighlight = async (lng, lat) => {
-    // The Tilequery API returns the least distance from the queried point first, so limiting to one will respond with the closest highlight
+    // The Tilequery API returns the least distance from the queried point first, so limiting to one 
+    // will respond with the closest highlight
     await axios.get(`https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/${lng},${lat}.json?radius=50&limit=1&layers=poi_label&access_token=pk.eyJ1IjoiZmlzaHN0aXh6IiwiYSI6ImNrems3b2p0MzIzZmgybm9jcjVzMHZ1aHoifQ.s6imzJ1hBpWdfdJzwmehOQ`)
     .then(response => {
         data = response.data.features;
--- part2/index.js
+++ part2/index.js
@@ -6,18 +6,25 @@ const controller = require('./controllers/methods')
 const app = express();
 const port = 5000;
 
+// Enforce usage of Cross-Origin Resource Sharing
 app.use(cors());
+
+// Enforce JSON type on response body
 app.use(bodyParser.json());
 
+// Highlights route
 app.get('/api/highlights/:dep/:arr', async (req, res) => {
     console.log(`Request with dep=${req.params.dep} and arr=${req.params.arr}`);
+    // Get all coordinates considered along the path between dep and arr
     const response = await controller.getDirections(req.params.dep, req.params.arr);
     const coors = response.geometry.coordinates;
     let highlights = [];
-    let loop = new Promise((resolve, reject) => {
+    // For each coordinate search for the relative highlights objects and add them to an array
+    let loop = new Promise((resolve) => {
         coors.forEach(async (coor, index, coors) => {
             const features = await controller.getHighlights(coor[0], coor[1]);
             features.forEach(feature => {
+                // Enforce that if certain values do not exist these will be changed to None
                 highlights.push({
                     'POICoordinates': (!('coordinates' in feature.geometry)) ? 'None' : feature.geometry.coordinates,
                     'Name': (!('name' in feature.properties)) ? 'None' : feature.properties.name,
@@ -26,6 +33,7 @@ app.get('/api/highlights/:dep/:arr', async (req, res) => {
                     'Category': (!('category_en' in feature.properties)) ? 'None' : feature.properties.category_en,
                 });
             });
+            // End the promise when the code reaches the end of the list of coordinates
             if(index == coors.length -1) resolve(); 
         });
     });
@@ -34,19 +42,31 @@ app.get('/api/highlights/:dep/:arr', async (req, res) => {
     });
     loop.catch((err) => {
         console.error(err);
+        res.status(err.status || 500).json({'error': err});
     });
 });
 
+// Closest highlight route
 app.get('/api/closest/:lng/:lat', async (req, res) => {
     console.log(`Request with lng=${req.params.lng} and lat=${req.params.lat}`);
+    // Receive closest highlight object to the given coordinate
     const response = await controller.closestHighlight(req.params.lng, req.params.lat);
-    res.json({
-        'POICoordinates': (!('coordinates' in response[0].geometry)) ? 'None' : response[0].geometry.coordinates,
-        'Name': (!('name' in response[0].properties)) ? 'None' : response[0].properties.name,
-        'Type': (!('type' in response[0].properties)) ? 'None' : response[0].properties.type,
-        'Class': (!('class' in response[0].properties)) ? 'None' : response[0].properties.class,
-        'Category': (!('category_en' in response[0].properties)) ? 'None' : response[0].properties.category_en,
-    });
+    console.log(response[0]);
+    // If there are no closeby highlights send an error to the user
+    if(typeof(response[0]) === 'undefined') { 
+        console.error('No available results for the asked coordinates.');
+        res.status(503).json({'error': 'No available results for the asked coordinates.'})
+    }
+    // Else populate the response with an object containing information on the closest highlight
+    else {
+        res.json({
+            'POICoordinates': (!('coordinates' in response[0].geometry)) ? 'None' : response[0].geometry.coordinates,
+            'Name': (!('name' in response[0].properties)) ? 'None' : response[0].properties.name,
+            'Type': (!('type' in response[0].properties)) ? 'None' : response[0].properties.type,
+            'Class': (!('class' in response[0].properties)) ? 'None' : response[0].properties.class,
+            'Category': (!('category_en' in response[0].properties)) ? 'None' : response[0].properties.category_en,
+        });
+    };
 });
 
 app.listen(port, () => {
